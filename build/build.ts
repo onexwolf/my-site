394aff9e697005f62790e41c73fd892879d4d842
liangxwei <liangxwei1030@163.com>
2020-09-15 20:10:11 +0800

    change style

--- build/build.ts
+++ build/build.ts
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const dirTree = require('directory-tree');
 
-const tree = dirTree('./src/assets/notes', { extension: /\.md/ });
+const tree = dirTree('./src/assets/notes', { extension: /\.md/, exclude: /DS_Store/ });
 
-fs.writeFileSync('./src/app/notes/data/articles.json', JSON.stringify(tree, null, 2), 'utf-8');
+fs.writeFileSync('./src/app/notes/data/notes.json', JSON.stringify(tree, null, 2), 'utf-8');
